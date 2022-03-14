50a1c1c0a0ebc1496dcb35c2fa58057b8daef66b
Adrià Hernández Morell <adriahernandez.2001@gmail.com>
2022-03-14 20:55:58 +0100

    Finished v.1.0

--- src/App.js
+++ src/App.js
@@ -30,48 +30,141 @@ const App = () => {
     }
 
     return (
-        <div className='container'>
-            <div className='background'></div>
-            <Navbar />
+        <div>
 
-            <section className='title'>
-                <h1>HEX CRYPTOPUNX</h1>
-            </section>
+        {!isMobile &&
+            <div className='container'>
+                <div className='background'></div>
+                <Navbar />
 
-            <section className='form-section'>
-                <div>
-                    <img src={punx_gif} alt="cryptopunx gif" />
-                </div>
-                <span>MINT PRICE - 0.0169 ETH</span>
-                <span>TOTAL -/10000</span>
-                <form className='mint-form' onSubmit={handleSubmit}>
-                    <input 
-                        type="number"
-                        min="1"
-                        max="30"
-                        id='mint-amount-counter'
-                        className='mint-amount-counter'
-                        name='amount_to_mint'
-                        value={amountToMint}
-                        onChange={(e) => {
-                            setAmountToMint(e.target.value)
-                        }}
-                    />
-                    <input type="submit" className='submit-amount_to_mint' value="MINT" />
-                </form>
-            </section>
+                <section className='title'>
+                    <h1>HEX CRYPTOPUNX</h1>
+                </section>
 
-            <section className='text-section'>
-                <div className='text-div'>
-                    <p>The idea for Hex Cryptopunx came from the release of Twitters hexagon nft profile pics. The project is being stealth launched with no need for discord grinding or giveaways for whitelist spots. Just a good old classic public mint. No max mint per transaction, no max per wallet.</p>
-                    <p>The collection consists of 10,000 randomly generated nfts on the ethereum blockchain. Several traits have been removed from the original Cryptopunks attributes list and a few new traits introduced. Additionally, some traits that were gender specific will now be available for male and females.</p>
-                    <p>What are our plans after launch? Well, we have some ideas but will take community feedback into account for what you would like to see! Future drops will be free to claim for Hex Cryptopunx holders!</p>
-                </div>
-                <div className='preview-div'>
-                    <img src={preview_gif} alt="cryptopunx preview gif" />
-                </div>
-            </section>
+                <section className='form-section'>
+                    <div>
+                        <img src={punx_gif} alt="cryptopunx gif" />
+                    </div>
+                    <span>MINT PRICE - 0.0169 ETH</span>
+                    <span>TOTAL -/10000</span>
+                    <form className='mint-form' onSubmit={handleSubmit}>
+                        <input
+                            type="number"
+                            min="1"
+                            max="30"
+                            id='mint-amount-counter'
+                            className='mint-amount-counter'
+                            name='amount_to_mint'
+                            value={amountToMint}
+                            onChange={(e) => {
+                                setAmountToMint(e.target.value)
+                            }}
+                            />
+                        <input type="submit" className='submit-amount_to_mint' value="MINT" />
+                    </form>
+                </section>
+
+                <section className='text-section'>
+                    <div className='text-div'>
+                        <p>The idea for Hex Cryptopunx came from the release of Twitters hexagon nft profile pics. The project is being stealth launched with no need for discord grinding or giveaways for whitelist spots. Just a good old classic public mint. No max mint per transaction, no max per wallet.</p>
+                        <p>The collection consists of 10,000 randomly generated nfts on the ethereum blockchain. Several traits have been removed from the original Cryptopunks attributes list and a few new traits introduced. Additionally, some traits that were gender specific will now be available for male and females.</p>
+                        <p>What are our plans after launch? Well, we have some ideas but will take community feedback into account for what you would like to see! Future drops will be free to claim for Hex Cryptopunx holders!</p>
+                    </div>
+                    <div className='preview-div'>
+                        <img src={preview_gif} alt="cryptopunx preview gif" />
+                    </div>
+                </section>
+            </div>
+        }
+        {isMobile &&
+            <div className='container_M'>
+                <div className='background_M'></div>
+                <Navbar />
+
+                <section className='title_M'>
+                    <h1 className='h1_M'>HEX CRYPTOPUNX</h1>
+                </section>
+
+                <section className='form-section_M'>
+                    <div>
+                        <img src={punx_gif} alt="cryptopunx gif" />
+                    </div>
+                    <span>MINT PRICE - 0.0169 ETH</span>
+                    <span>TOTAL -/10000</span>
+                    <form className='mint-form_M' onSubmit={handleSubmit}>
+                        <input
+                            type="number"
+                            min="1"
+                            max="30"
+                            id='mint-amount-counter'
+                            className='mint-amount-counter_M'
+                            name='amount_to_mint'
+                            value={amountToMint}
+                            onChange={(e) => {
+                                setAmountToMint(e.target.value)
+                            }}
+                        />
+                        <input type="submit" className='submit-amount_to_mint_M' value="MINT" />
+                    </form>
+                </section>
+
+                <section className='text-section_M'>
+                    <div className='text-div_M'>
+                        <p>The idea for Hex Cryptopunx came from the release of Twitters hexagon nft profile pics. The project is being stealth launched with no need for discord grinding or giveaways for whitelist spots. Just a good old classic public mint. No max mint per transaction, no max per wallet.</p>
+                        <p>The collection consists of 10,000 randomly generated nfts on the ethereum blockchain. Several traits have been removed from the original Cryptopunks attributes list and a few new traits introduced. Additionally, some traits that were gender specific will now be available for male and females.</p>
+                        <p>What are our plans after launch? Well, we have some ideas but will take community feedback into account for what you would like to see! Future drops will be free to claim for Hex Cryptopunx holders!</p>
+                    </div>
+                    <div className='preview-div_M'>
+                        <img src={preview_gif} alt="cryptopunx preview gif" />
+                    </div>
+                </section>
+            </div>
+        }
+
         </div>
+
+        // <div className='container'>
+        //     <div className='background'></div>
+        //     <Navbar />
+
+        //     <section className='title'>
+        //         <h1>HEX CRYPTOPUNX</h1>
+        //     </section>
+
+        //     <section className='form-section'>
+        //         <div>
+        //             <img src={punx_gif} alt="cryptopunx gif" />
+        //         </div>
+        //         <span>MINT PRICE - 0.0169 ETH</span>
+        //         <span>TOTAL -/10000</span>
+        //         <form className='mint-form' onSubmit={handleSubmit}>
+        //             <input 
+        //                 type="number"
+        //                 min="1"
+        //                 max="30"
+        //                 id='mint-amount-counter'
+        //                 className='mint-amount-counter'
+        //                 name='amount_to_mint'
+        //                 value={amountToMint}
+        //                 onChange={(e) => {
+        //                     setAmountToMint(e.target.value)
+        //                 }}
+        //             />
+        //             <input type="submit" className='submit-amount_to_mint' value="MINT" />
+        //         </form>
+        //     </section>
+
+        //     <section className='text-section'>
+        //         <div className='text-div'>
+        //             <p>The idea for Hex Cryptopunx came from the release of Twitters hexagon nft profile pics. The project is being stealth launched with no need for discord grinding or giveaways for whitelist spots. Just a good old classic public mint. No max mint per transaction, no max per wallet.</p>
+        //             <p>The collection consists of 10,000 randomly generated nfts on the ethereum blockchain. Several traits have been removed from the original Cryptopunks attributes list and a few new traits introduced. Additionally, some traits that were gender specific will now be available for male and females.</p>
+        //             <p>What are our plans after launch? Well, we have some ideas but will take community feedback into account for what you would like to see! Future drops will be free to claim for Hex Cryptopunx holders!</p>
+        //         </div>
+        //         <div className='preview-div'>
+        //             <img src={preview_gif} alt="cryptopunx preview gif" />
+        //         </div>
+        //     </section>
+        // </div>
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 import "./Navbar.css"
+import { isMobile } from 'react-device-detect';
 // import NavItems from './NavItems'
 import SocialMedia from './SocialMedia'
 // import Metamask from "../../assets/logos/metamask-fox.svg"
@@ -15,19 +16,39 @@ import SocialMedia from './SocialMedia'
 const Navbar = ({}) => {
  
     return (
-        <div className='navbar'>
-            <div className='nav-social-media'>
-                {SocialMedia.socialmedia.map((mediaitem) => (
-                    <a id={mediaitem.name} href={mediaitem.url} target="_blank" rel="noreferrer">
-                        {(mediaitem.iconType === 'fontawesome') &&
-                            <FontAwesomeIcon icon={mediaitem.icon} className='nav-icon'></FontAwesomeIcon>
-                        }
-                        {(mediaitem.iconType === 'svg') &&
-                            <img src={mediaitem.icon} alt={mediaitem.name} className='nav-icon'/>
-                        }
-                    </a>
-                ))}
-            </div>
+        <div>
+            {!isMobile &&
+                <div className='navbar'>
+                    <div className='nav-social-media'>
+                        {SocialMedia.socialmedia.map((mediaitem) => (
+                            <a id={mediaitem.name} href={mediaitem.url} target="_blank" rel="noreferrer">
+                                {(mediaitem.iconType === 'fontawesome') &&
+                                    <FontAwesomeIcon icon={mediaitem.icon} className='nav-icon'></FontAwesomeIcon>
+                                }
+                                {(mediaitem.iconType === 'svg') &&
+                                    <img src={mediaitem.icon} alt={mediaitem.name} className='nav-icon'/>
+                                }
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            }
+            {isMobile &&
+                <div className='navbar_M'>
+                    <div className='nav-social-media_M'>
+                        {SocialMedia.socialmedia.map((mediaitem) => (
+                            <a id={mediaitem.name} href={mediaitem.url} target="_blank" rel="noreferrer">
+                                {(mediaitem.iconType === 'fontawesome') &&
+                                    <FontAwesomeIcon icon={mediaitem.icon} className='nav-icon_M'></FontAwesomeIcon>
+                                }
+                                {(mediaitem.iconType === 'svg') &&
+                                    <img src={mediaitem.icon} alt={mediaitem.name} className='nav-icon_M' />
+                                }
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            }
         </div>
     )
 }
